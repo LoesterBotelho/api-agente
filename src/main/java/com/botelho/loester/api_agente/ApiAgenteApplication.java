ff6ca8d48ea860de405ead166b67ff0750982f24
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-09-04 19:55:45 -0300

    first commit

---
+++ src/main/java/com/botelho/loester/api_agente/ApiAgenteApplication.java
@@ -0,0 +1,13 @@
+package com.botelho.loester.api_agente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botelho/loester/api_agente/ApiAgenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botelho.loester.api_agente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
